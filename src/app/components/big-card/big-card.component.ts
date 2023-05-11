4f1af61b75f533e97fbbdadbc4baf9862d592ba6
GusRMatos <reisgustavo08@gmail.com>
2023-05-11 17:12:24 -0300

    funcionalidades passadas no curso completas

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css','./big-card.responsive.component.css']
+})
+export class BigCardComponent implements OnInit{
+  
+  @Input()
+  photoCover: string = ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardContent: string = ""
+  @Input()
+  cardDate: string = ""
+  @Input()
+  id: string = "0"
+
+  constructor (){ }
+  
+  ngOnInit(): void {
+  
+  }
+
+}
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo":"https://inforchannel.com.br/wp-content/uploads/2023/02/Github-Top-10-linguagens-de-programacao-1024x576.jpg",
+        "title": "Top Linguagens de programação 2022",
+        "content": "Em um mundo moderno cada vez mais dependente de plataformas digitais, profissionais de programação estão vendo crescer sua demanda e, com ela, a variedade de particularidades de cada projeto. Em 2022"
+    },
+    {
+        "id":"2",
+        "photo":"https://i0.wp.com/oportaln10.com.br/wp-content/uploads/2019/01/javascript.jpg?fit=800%2C450&ssl=1://inforchannel.com.br/wp-content/uploads/2023/02/Github-Top-10-linguagens-de-programacao-1024x576.jpg",
+        "title": "Sobre javascript",
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis molestias magnam tempora qui doloremque dicta quasi mollitia, numquam sunt nihil ab quidem pariatur laborum quia. Id beatae minima deserunt illum!"
+    },
+    {
+        "id":"3",
+        "photo":"https://raw.githubusercontent.com/khaosdoctor/blog-assets/master/images/2022/06/10b88c68-typescript-logo.png",
+        "title": "Sobre TypeScript",
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis molestias magnam tempora qui doloremque dicta quasi mollitia, numquam sunt nihil ab quidem pariatur laborum quia. Id beatae minima deserunt illum!"
+    }
+]
